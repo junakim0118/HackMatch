0c647fc67cf14d0f46d46e828f22caaefbf666ab
Nathan <nwan23@uwo.ca>
2024-11-30 13:15:47 -0500

    Fix overlap

--- hackmatch/src/App.js
+++ hackmatch/src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Signup from './pages/Signup'; 
-import Name from './pages/AccountCreation/Name.js';  
-import Links from './pages/AccountCreation/Links.js';  
-import Bio from './pages/AccountCreation/bio.js';
-import Fun from './pages/AccountCreation/fun.js';
-import Signup from './pages/Signup';  // Your Signup component
-import AccountCreation from './pages/AccountCreation';  // Your AccountCreation component
-import Home from './pages/home';
-import Signup from './pages/Signup';  // Your Signup component
-import AccountCreation from './pages/AccountCreation';  // Your AccountCreation component
-import Home from './pages/home';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Name from "./pages/AccountCreation/Name.js";
+import Links from "./pages/AccountCreation/Links.js";
+import Bio from "./pages/AccountCreation/bio.js";
+import Fun from "./pages/AccountCreation/fun.js";
+// import Signup from "./pages/Signup"; // Your Signup component
+// import AccountCreation from "./pages/AccountCreation"; // Your AccountCreation component
+import Home from "./pages/home";
+// import Signup from "./pages/Signup"; // Your Signup component
+// import AccountCreation from "./pages/AccountCreation"; // Your AccountCreation component
+// import Home from "./pages/home";
 
 function App() {
   return (
@@ -22,7 +22,7 @@ function App() {
         <Route path="/Bio" element={<Bio />} />
         <Route path="/Fun" element={<Fun />} />
         <Route path="/" element={<Home />} />
-        <Route path="/accountCreation" element={<AccountCreation />} />
+        {/* <Route path="/accountCreation" element={<AccountCreation />} /> */}
       </Routes>
     </Router>
   );
