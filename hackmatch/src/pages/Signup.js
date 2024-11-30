ee488a05e0f2a35e3568ea2abd661a0e61571e24
Nathan <nwan23@uwo.ca>
2024-11-30 03:21:31 -0500

    some

--- hackmatch/src/pages/Signup.js
+++ hackmatch/src/pages/Signup.js
@@ -8,6 +8,16 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your Firebase config
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBFCkPH2ZbloXAo4rpztmCPQe0zoFiopXQ",
+  authDomain: "hackmatch-9fef5.firebaseapp.com",
+  projectId: "hackmatch-9fef5",
+  storageBucket: "hackmatch-9fef5.firebasestorage.app",
+  messagingSenderId: "520362196145",
+  appId: "1:520362196145:web:338074b520500558317690",
+  measurementId: "G-SZV8RS906G",
+};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
