9f2c772e8efe88d9b941fd7452a7bf576777305e
Nathan <nwan23@uwo.ca>
2024-11-30 13:11:06 -0500

    Account page

--- hackmatch/src/App.js
+++ hackmatch/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup"; // Your Signup component
 import AccountCreation from "./pages/AccountCreation"; // Your AccountCreation component
+import AccountPage from "./pages/AccountPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<AccountPage />} />
         <Route path="/accountCreation" element={<AccountCreation />} />
       </Routes>
     </Router>
