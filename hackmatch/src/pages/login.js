422f19367dd892b394cf784a200134bd81af30fd
jenusany <jenusany@outlook.com>
2024-11-30 01:25:31 -0500

    added pages folder

---
+++ hackmatch/src/pages/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
