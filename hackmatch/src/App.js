8bd7f80f67df14811a19df73ee8002cf7657825d
Juna <115675811+junakim0118@users.noreply.github.com>
2024-11-30 04:14:18 -0500

    homepage set up done

--- hackmatch/src/App.js
+++ hackmatch/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './pages/Signup';  // Your Signup component
 import AccountCreation from './pages/AccountCreation';  // Your AccountCreation component
+import Home from './pages/home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<Home />} />
         <Route path="/accountCreation" element={<AccountCreation />} />
       </Routes>
     </Router>
